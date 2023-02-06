c2afb0a591f658ad3e9dc6e324f424aafcde5c62
banana-brainy <krokodeall124@gmail.com>
2023-02-06 19:05:33 +0200

    fixing unnecessary syntax

--- src/components/Scroll.tsx
+++ src/components/Scroll.tsx
@@ -5,7 +5,7 @@ interface IScrollProps {
     children: JSX.Element
 }
 
-const Scroll: FC<IScrollProps> = (props, children) => {
+const Scroll: FC<IScrollProps> = (props) => {
     return (
         <div style={{overflowY:'scroll', border: '1px solid black', height: '900px'}}>
             {props.children}
